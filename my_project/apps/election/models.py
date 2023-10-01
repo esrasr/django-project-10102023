8531c81409d507d20dae2ce82ee5cbf766941cb7
esrasr <esrasertdemir95@gmail.com>
2023-10-01 12:55:06 +0300

    model and initial data added

--- my_project/apps/election/models.py
+++ my_project/apps/election/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from apps.base.models import BaseModel
 
-# Create your models here.
+
+class Country(BaseModel):
+    name = models.CharField(max_length=100)
+    short_name = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = "country"
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class City(BaseModel):
+    name = models.CharField(max_length=100)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "city"
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Candidates(BaseModel):
+    name = models.CharField(max_length=100)
+    party = models.CharField(max_length=20)
+    # image
+
+    class Meta:
+        db_table = "candidates"
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Election(BaseModel):
+    candidate = models.ForeignKey(Candidates, on_delete=models.CASCADE)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    decription = models.CharField(max_length=100)
+    count = models.IntegerField()
+
+    class Meta:
+        db_table = "election"
