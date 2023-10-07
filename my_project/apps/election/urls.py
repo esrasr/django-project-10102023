dab2d9fb94642ae00570eed861e8203f3a3f98fe
esrasr <esrasertdemir95@gmail.com>
2023-10-07 18:41:55 +0300

    election detail page created

--- my_project/apps/election/urls.py
+++ my_project/apps/election/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from apps.election.views import index, get_city_ajax, get_city_chart_ajax, get_chart_ajax, election_details, pie_chart
+from apps.election.views import index, election_details, get_city_ajax, get_city_chart_ajax, get_chart_ajax, election_table
 
 
 app_name = "election"
@@ -12,5 +12,5 @@ urlpatterns = [
     path("get-chart-ajax/", get_chart_ajax, name="get_chart_ajax"),
     path("get-city-chart-ajax/", get_city_chart_ajax, name="get_city_chart_ajax"),
     path("election-details/<int:city_id>", election_details, name="election_details"),
-    path("charts/", pie_chart, name="charts"),
+    path("election-table", election_table, name="election_table"),
 ]
--- my_project/apps/election/views.py
+++ my_project/apps/election/views.py
@@ -6,6 +6,7 @@ from django.http import JsonResponse
 from django.contrib import messages
 from apps.election.utils import get_pie_chart
 from django.template.loader import render_to_string
+from django.db.models import Sum, Count, F
 
 
 def index(request):
@@ -61,27 +62,35 @@ def get_city_chart_ajax(request):
         return JsonResponse({"template_content": template_content})
 
 
+def election_table(request):
+    election_detail = (
+        Election.objects.select_related("city__country")
+        .values("city")
+        .annotate(
+            candidate_total_count=Sum("count"),
+            candidate_count=Count("candidate_id"),
+            country_name=F("city__country__name"),
+            city_name=F("city__name"),
+        )
+        .order_by("country_name", "city_name")
+        .values("country_name", "city_name", "candidate_total_count", "candidate_count", "city_id")
+    )
+    context = {"election_detail": election_detail}
+    return render(request, "election_table.html", context)
+
+
 def election_details(request, city_id):
     total_count = Election.objects.city_total_count(city_id=city_id)
     if total_count == 0:
-        messages.error(request, "This city does not exist.")
-        return HttpResponseRedirect(reverse("election:homepage"))
+        return JsonResponse({"template_content": ""})
+
     election_group = Election.objects.get_city_percent(city_id=city_id, total_count=total_count)
-    context = {"total_count": total_count, "election_group": election_group}
-    return render(request, "detail.html", context)
-
-
-def pie_chart(request):
-    total_count = Election.objects.country_total_count(country_id=4)
-
-    election_group = Election.objects.get_country_percent(country_id=4, total_count=total_count)
     labels = []
     sizes = []
     for item in election_group:
         labels.append(item["candidate__name"])
         sizes.append(item["percent"])
-
     image = get_pie_chart(labels, sizes)
-
-    context = {"total_count": total_count, "election_group": election_group, "graphic": image}
-    return render(request, "charts.html", context)
+    instance = Election.objects.filter(city_id=city_id).order_by("-count").all()[:10]
+    context = {"total_count": total_count, "election_group": election_group, "img_base64": image, "winner_name": election_group[0], "result": election_group[1:], "table": instance}
+    return render(request, "detail.html", context)
